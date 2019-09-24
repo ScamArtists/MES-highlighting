5c87661193c4c851f5d217649c16ffc999a34384
The947thTokay <the947thtokay@codingtime.nl>
2019-09-24 18:16:11 +0200

    Finish installer for nano

--- installer.sh
+++ installer.sh
@@ -1,11 +1,34 @@
 #!/usr/bin/env sh
 
+# Configuration files
+nano="/etc/nanorc"
+
 case "$1" in
     "i" | "install")
-        # TODO
+        case "$2" in
+            "nano")
+                echo "include \"$(pwd)/mes.nanorc\"" >> $nano
+            ;;
+            *)
+                echo "Unknown editor!"
+            ;;
+        esac
     ;;
     "u" | "uninstall")
-        # TODO
+        case "$2" in
+            "nano")
+                line=`grep -n "include \"$(pwd)/mes.nanorc\"" $nano | cut -d: -f1 | cut -d$'\n' -f1`
+                if [ -z "$line" ]
+                then
+                    echo "No installation found!"
+                else
+                    sed -i -e "${line}d" $nano
+                fi
+            ;;
+            *)
+                echo "Unknown editor!"
+            ;;
+        esac
     ;;
     *)
         echo "This is the MathEnslaver Highlighting build tool. It installs or removes the syntax highlighting from editors."
