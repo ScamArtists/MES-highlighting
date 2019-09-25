b3293e072f15ad6c43e49e6129caad3831b25200
The947thTokay <the947thtokay@codingtime.nl>
2019-09-25 17:07:33 +0200

    Make stuff consistent

--- installer.sh
+++ installer.sh
@@ -34,9 +34,9 @@ case "$1" in
         echo "This is the MathEnslaver Highlighting build tool. It installs or removes the syntax highlighting from editors."
         echo "Usage: ./installer.sh [action] [editor]"
         echo "Actions:"
-        echo "	i, install      | Install the highlighting"
-        echo "	u, uninstall    | Remove the highlighting"
+        echo "    i, install      | Install the highlighting"
+        echo "    u, uninstall    | Remove the highlighting"
         echo "Editors:"
-        echo "  nano"
+        echo "    nano"
     ;;
 esac
